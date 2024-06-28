9c7e2f0e3de7c1eaa409def4aabdad34b08adcaa
James Tuckett <james@tuckett.me>
2024-06-28 12:34:56 +0100

    test: create AaveV3Ark test file

--- src/contracts/arks/AaveV3Ark.sol
+++ src/contracts/arks/AaveV3Ark.sol
@@ -5,14 +5,15 @@ import "../Ark.sol";
 import {IPoolV3} from "../../interfaces/aave-v3/IPoolV3.sol";
 
 contract AaveV3Ark is Ark {
-    IPoolV3 public aavePool;
+    IPoolV3 public aaveV3Pool;
+    event Board(address indexed token, uint256 amount, address indexed onBehalfOf);
 
-    constructor(address _aavePool, ArkParams memory _params) Ark(_params) {
-        aavePool = IPoolV3(_aavePool);
+    constructor(address _aaveV3Pool, ArkParams memory _params) Ark(_params) {
+        aaveV3Pool = IPoolV3(_aaveV3Pool);
     }
 
     function board(uint256 amount) external override onlyCommander {
-        aavePool.supply(address(token), amount, address(this), 0);
+        aaveV3Pool.supply(address(token), amount, address(this), 0);
     }
 
     function disembark(uint256 amount) external override onlyCommander {}
---
+++ test/AaveV3Ark.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import "../src/contracts/arks/AaveV3Ark.sol";
+import "../src/errors/ArkAccessControlErrors.sol";
+import "openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
+import "./mocks/MockAaveV3Pool.sol";
+
+contract AaveV3ArkTest is Test {
+    AaveV3Ark public ark;
+    address public governor = address(1);
+    address public commander = address(4);
+    address public raft = address(2);
+    IPoolV3 public aaveV3Pool;
+    ERC20Mock public mockToken;
+
+    event Board(address indexed token, uint256 amount, address indexed onBehalfOf);
+
+
+    function setUp() public {
+        mockToken = new ERC20Mock();
+        aaveV3Pool = IPoolV3(address(new MockAavePool()));
+
+        ArkParams memory params = ArkParams({governor: governor, raft: raft, token: address(mockToken)});
+        ark = new AaveV3Ark(address(aaveV3Pool), params);
+    }
+
+    function testBoardArk() public {
+        vm.prank(governor); // Set msg.sender to governor
+        ark.grantCommanderRole(commander);
+
+        // Arrange
+        uint256 amount = 1000 * 10**18;
+        mockToken.mint(address(this), amount);
+        mockToken.approve(address(ark), amount);
+
+        vm.mockCall(
+            address(aaveV3Pool),
+            abi.encodeWithSelector(aaveV3Pool.supply.selector, address(mockToken), amount, address(this), 0),
+            abi.encode()
+        );
+
+        vm.expectCall(
+            address(aaveV3Pool),
+            abi.encodeWithSelector(aaveV3Pool.supply.selector, address(mockToken), amount, address(ark), 0)
+        );
+
+        // Act
+        vm.prank(commander); // Execute the next call as the commander
+        ark.board(amount);
+    }
+}
