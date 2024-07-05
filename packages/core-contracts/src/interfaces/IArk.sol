20ea17cd310ff61ac66efc0559932b06723fbc0d
halaprix <panicz.klos@gmail.com>
2024-07-05 11:07:38 +0200

    chore: add view functions docs

--- packages/core-contracts/src/interfaces/IArk.sol
+++ packages/core-contracts/src/interfaces/IArk.sol
@@ -9,8 +9,11 @@ import "./IArkEvents.sol";
 
 interface IArk is IArkAccessControl, IArkEvents {
     /* FUNCTIONS - PUBLIC */
+
+    /// @notice Returns the current underlying balance of the Ark (token precision)
     function balance() external view returns (uint256);
 
+    /// @notice Returns the current rate of the Ark (RAY precision)
     function rate() external view returns (uint256);
 
     function harvest() external;
