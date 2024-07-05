e7a17b2ed97e38c6b13b89301652f781620aecca
James Tuckett <james@tuckett.me>
2024-07-05 10:25:11 +0200

    feat: create outline for configuration manager contract

---
+++ packages/core-contracts/src/contracts/ConfigurationManager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import {IConfigurationManager} from "../interfaces/IConfigurationManager.sol";
+
+
+/**
+ * @custom:see IConfigurationManager
+ */
+contract ConfigurationManager is IConfigurationManager {
+
+}
