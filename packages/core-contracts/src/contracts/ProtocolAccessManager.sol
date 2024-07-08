f854de6d665d3b6d4061adacad7901654fb80b62
James Tuckett <james@tuckett.me>
2024-07-08 20:23:28 +0100

    feat: create ProtocolAccessManager.sol and Roles.sol

---
+++ packages/core-contracts/src/contracts/ProtocolAccessManager.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import {AccessManager} from "@openzeppelin/contracts/access/manager/AccessManager.sol";
+
+contract ProtocolAccessManager is AccessManager {
+
+}
