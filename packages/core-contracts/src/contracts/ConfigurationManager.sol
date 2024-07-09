4713b2ee7e13342ec68f42ebe25f6adac12bd8fb
James Tuckett <james@tuckett.me>
2024-07-09 09:15:20 +0100

    refactor: update ConfigurationManager to use ProtocolAccessManaged.sol

--- packages/core-contracts/src/contracts/ConfigurationManager.sol
+++ packages/core-contracts/src/contracts/ConfigurationManager.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.26;
 
 import {IConfigurationManager} from "../interfaces/IConfigurationManager.sol";
 import {ConfigurationManagerParams} from "../types/ConfigurationManagerTypes.sol";
-import {AccessManaged} from "@openzeppelin/contracts/access/manager/AccessManaged.sol";
+import {ProtocolAccessManaged} from "./ProtocolAccessManaged.sol";
 
 /**
  * @custom:see IConfigurationManager
  */
-contract ConfigurationManager is IConfigurationManager, AccessManaged {
+contract ConfigurationManager is IConfigurationManager, ProtocolAccessManaged {
     /**
      * @notice The Rewards And Farmed Tokens contract. It is where rewards and farmed tokens are
      *         sent for processing
@@ -17,11 +17,11 @@ contract ConfigurationManager is IConfigurationManager, AccessManaged {
 
     constructor(
         ConfigurationManagerParams memory _params
-    ) AccessManaged(_params.accessManager) {
+    ) ProtocolAccessManaged(_params.accessManager) {
         raft = _params.raft;
     }
 
-    function setRaft(address newRaft) external restricted {
+    function setRaft(address newRaft) external onlyGovernor {
         raft = newRaft;
 
         emit RaftUpdated(newRaft);
