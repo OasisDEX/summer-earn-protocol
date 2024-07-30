67d1dd3d1a0868ecce9b9f1e3d72246e4ba4ff7f
James Tuckett <james@tuckett.me>
2024-07-30 14:55:11 +0100

    feat: commit the TipJar contract outlines

---
+++ packages/core-contracts/src/contracts/TipJar.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+import {ITipJar} from "../interfaces/ITipJar.sol";
+
+/**
+ * @title TipJar
+ * @notice Contract implementing the centralised collection of Tips
+ */
+contract TipJar is ITipJar {
+
+
+}
---
+++ packages/core-contracts/src/interfaces/ITipJar.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.26;
+
+/**
+ * @title ITipJar
+ * @notice Interface for the TipJar contract
+ */
+interface ITipJar {
+
+}
