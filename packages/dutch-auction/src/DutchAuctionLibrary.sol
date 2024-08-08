63bf698f6a4c6d977651d1b3b624287587358438
halaprix <panicz.klos@gmail.com>
2024-08-08 15:16:40 +0200

    chore: remove unused import

--- packages/dutch-auction/src/DutchAuctionLibrary.sol
+++ packages/dutch-auction/src/DutchAuctionLibrary.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./DutchAuctionErrors.sol";
 import "./DutchAuctionEvents.sol";
 
