d6f56ea01fef66432eafca0c8474039589f6f48b
halaprix <panicz.klos@gmail.com>
2024-08-13 14:14:40 +0200

    chore: add percentage package imports to dutch auction

--- packages/dutch-auction/src/DutchAuctionLibrary.sol
+++ packages/dutch-auction/src/DutchAuctionLibrary.sol
@@ -9,8 +9,8 @@ import "./DutchAuctionMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import {Percentage} from "./lib/Percentage.sol";
-import {PercentageUtils} from "./lib/PercentageUtils.sol";
+import {Percentage} from "@summerfi/percentage/src/Percentage.sol";
+import {PercentageUtils} from "@summerfi/percentage/src/PercentageUtils.sol";
 
 /**
  * @title Dutch Auction Library
