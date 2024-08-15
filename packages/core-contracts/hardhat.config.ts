a15e0304a93473689e64691d13b89f5a6ff6c304
James Tuckett <james@tuckett.me>
2024-08-15 14:10:26 +0100

    feat: add HH viem ignition

--- packages/core-contracts/hardhat.config.ts
+++ packages/core-contracts/hardhat.config.ts
@@ -1,6 +1,7 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox"
 import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-ignition-viem";
 
 const config: HardhatUserConfig = {
   solidity: {
