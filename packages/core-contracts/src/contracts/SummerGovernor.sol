65dd48b90186343a28b6ee42c13e427fba2eb3ac
James Tuckett <james@tuckett.me>
2024-08-27 14:03:42 +0100

    test: fix up governance contract tests

--- packages/core-contracts/src/contracts/SummerGovernor.sol
+++ packages/core-contracts/src/contracts/SummerGovernor.sol
@@ -6,20 +6,42 @@ import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract SummerEarnGovernor is
+/*
+ * @title SummerGovernor
+ * @dev This contract implements a governance system with additional features such as
+ * whitelisting, pausing, and custom proposal thresholds.
+ * It extends various OpenZeppelin Governor contracts to provide a comprehensive
+ * governance solution.
+ */
+contract SummerGovernor is
     GovernorSettings,
     GovernorCountingSimple,
     GovernorVotes,
     GovernorVotesQuorumFraction,
-    GovernorTimelockControl
+    GovernorTimelockControl,
+    Pausable
 {
+    /* @dev Minimum number of tokens required to create a proposal */
     uint256 public constant MIN_PROPOSAL_THRESHOLD = 1000e18; // 1,000 Tokens
+    /* @dev Maximum number of tokens allowed for proposal threshold */
     uint256 public constant MAX_PROPOSAL_THRESHOLD = 100000e18; // 100,000 Tokens
 
+    /* @dev Mapping to store expiration timestamps for whitelisted accounts */
     mapping(address => uint256) public whitelistAccountExpirations;
+    /* @dev Address of the whitelist guardian who can cancel proposals */
     address public whitelistGuardian;
 
+    /*
+     * @dev Constructor to initialize the SummerGovernor contract
+     * @param _token The ERC20Votes token used for governance
+     * @param _timelock The TimelockController used for executing proposals
+     * @param _votingDelay The delay before voting on a proposal may take place, once proposed, in blocks
+     * @param _votingPeriod The period of voting for a proposal, in blocks
+     * @param _proposalThreshold The number of votes required in order for a voter to become a proposer
+     * @param _quorumFraction The fraction of total supply that should be present when voting on proposals
+     */
     constructor(
         IVotes _token,
         TimelockController _timelock,
@@ -28,11 +50,12 @@ contract SummerEarnGovernor is
         uint256 _proposalThreshold,
         uint256 _quorumFraction
     )
-        Governor("SummerEarnGovernor")
+        Governor("SummerGovernor")
         GovernorSettings(_votingDelay, _votingPeriod, _proposalThreshold)
         GovernorVotes(_token)
         GovernorVotesQuorumFraction(_quorumFraction)
         GovernorTimelockControl(_timelock)
+        Pausable()
     {
         require(
             _proposalThreshold >= MIN_PROPOSAL_THRESHOLD &&
@@ -41,6 +64,24 @@ contract SummerEarnGovernor is
         );
     }
 
+    /* @dev Pauses the contract. Can only be called by governance. */
+    function pause() public onlyGovernance {
+        _pause();
+    }
+
+    /* @dev Unpauses the contract. Can only be called by governance. */
+    function unpause() public onlyGovernance {
+        _unpause();
+    }
+
+    /*
+     * @dev Proposes a new governance action
+     * @param targets The ordered list of target addresses for calls to be made
+     * @param values The ordered list of values (i.e. msg.value) to be passed to the calls to be made
+     * @param calldatas The ordered list of function signatures to be called
+     * @param description A human readable description of the proposal
+     * @return The ID of the newly created proposal
+     */
     function propose(
         address[] memory targets,
         uint256[] memory values,
@@ -56,6 +97,14 @@ contract SummerEarnGovernor is
         return super.propose(targets, values, calldatas, description);
     }
 
+    /*
+     * @dev Executes a successful proposal
+     * @param targets The ordered list of target addresses for calls to be made
+     * @param values The ordered list of values (i.e. msg.value) to be passed to the calls to be made
+     * @param calldatas The ordered list of function signatures to be called
+     * @param descriptionHash The hash of the proposal description
+     * @return The ID of the executed proposal
+     */
     function execute(
         address[] memory targets,
         uint256[] memory values,
@@ -65,6 +114,14 @@ contract SummerEarnGovernor is
         return super.execute(targets, values, calldatas, descriptionHash);
     }
 
+    /*
+     * @dev Cancels a proposal
+     * @param targets The ordered list of target addresses for calls to be made
+     * @param values The ordered list of values (i.e. msg.value) to be passed to the calls to be made
+     * @param calldatas The ordered list of function signatures to be called
+     * @param descriptionHash The hash of the proposal description
+     * @return The ID of the canceled proposal
+     */
     function cancel(
         address[] memory targets,
         uint256[] memory values,
@@ -82,11 +139,12 @@ contract SummerEarnGovernor is
             _msgSender() == proposer ||
                 getVotes(proposer, block.number - 1) < proposalThreshold() ||
                 _msgSender() == whitelistGuardian,
-            "SummerEarnGovernor: only proposer, whitelisted proposer below threshold, or guardian"
+            "SummerGovernor: only proposer, whitelisted proposer below threshold, or guardian"
         );
-        return super.cancel(targets, values, calldatas, descriptionHash);
+        return super._cancel(targets, values, calldatas, descriptionHash);
     }
 
+    /* @dev Internal function to cancel a proposal */
     function _cancel(
         address[] memory targets,
         uint256[] memory values,
@@ -96,6 +154,7 @@ contract SummerEarnGovernor is
         return super._cancel(targets, values, calldatas, descriptionHash);
     }
 
+    /* @dev Returns the address of the executor (timelock) */
     function _executor()
         internal
         view
@@ -105,6 +164,7 @@ contract SummerEarnGovernor is
         return super._executor();
     }
 
+    /* @dev Returns the current proposal threshold */
     function proposalThreshold()
         public
         view
@@ -114,6 +174,7 @@ contract SummerEarnGovernor is
         return super.proposalThreshold();
     }
 
+    /* @dev Returns the state of a proposal */
     function state(
         uint256 proposalId
     )
@@ -125,16 +186,23 @@ contract SummerEarnGovernor is
         return super.state(proposalId);
     }
 
+    /* @dev Checks if the contract supports an interface */
     function supportsInterface(
         bytes4 interfaceId
     ) public view override(Governor) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
+    /* @dev Checks if an account is whitelisted */
     function isWhitelisted(address account) public view returns (bool) {
         return (whitelistAccountExpirations[account] > block.timestamp);
     }
 
+    /*
+     * @dev Sets the expiration for a whitelisted account
+     * @param account The address to whitelist
+     * @param expiration The timestamp when the whitelist expires
+     */
     function setWhitelistAccountExpiration(
         address account,
         uint256 expiration
@@ -143,6 +211,10 @@ contract SummerEarnGovernor is
         emit WhitelistAccountExpirationSet(account, expiration);
     }
 
+    /*
+     * @dev Sets the whitelist guardian
+     * @param _whitelistGuardian The address of the new whitelist guardian
+     */
     function setWhitelistGuardian(
         address _whitelistGuardian
     ) external onlyGovernance {
@@ -150,13 +222,18 @@ contract SummerEarnGovernor is
         emit WhitelistGuardianSet(_whitelistGuardian);
     }
 
+    /* @dev Event emitted when a whitelist account expiration is set */
     event WhitelistAccountExpirationSet(
         address indexed account,
         uint256 expiration
     );
+    /* @dev Event emitted when the whitelist guardian is set */
     event WhitelistGuardianSet(address indexed newGuardian);
 
-    // Override to resolve conflicts between Governor and GovernorTimelockControl
+    /*
+     * @dev Internal function to execute proposal operations
+     * Overrides to resolve conflicts between Governor and GovernorTimelockControl
+     */
     function _executeOperations(
         uint256 proposalId,
         address[] memory targets,
@@ -174,6 +251,10 @@ contract SummerEarnGovernor is
             );
     }
 
+    /*
+     * @dev Internal function to queue proposal operations
+     * Overrides to resolve conflicts between Governor and GovernorTimelockControl
+     */
     function _queueOperations(
         uint256 proposalId,
         address[] memory targets,
@@ -192,6 +273,7 @@ contract SummerEarnGovernor is
             );
     }
 
+    /* @dev Checks if a proposal needs queuing */
     function proposalNeedsQueuing(
         uint256 proposalId
     ) public view override(Governor, GovernorTimelockControl) returns (bool) {
--- packages/core-contracts/test/governance/SummerGovernor.t.sol
+++ packages/core-contracts/test/governance/SummerGovernor.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../../src/contracts/Governor.sol";
+import "../../src/contracts/SummerGovernor.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/governance/TimelockController.sol";
@@ -30,8 +30,8 @@ contract MockERC20Votes is ERC20, ERC20Permit, ERC20Votes {
     }
 }
 
-contract SummerEarnGovernorTest is Test {
-    SummerEarnGovernor public governor;
+contract SummerGovernorTest is Test {
+    SummerGovernor public governor;
     MockERC20Votes public token;
     TimelockController public timelock;
 
@@ -46,7 +46,12 @@ contract SummerEarnGovernorTest is Test {
     uint256 public constant QUORUM_FRACTION = 4;
 
     function setUp() public {
+        vm.label(alice, "Alice");
+        vm.label(bob, "Bob");
+        vm.label(charlie, "Charlie");
+
         token = new MockERC20Votes();
+        vm.label(address(token), "MockERC20Votes");
         token.mint(alice, INITIAL_SUPPLY);
 
         address[] memory proposers = new address[](1);
@@ -59,8 +64,9 @@ contract SummerEarnGovernorTest is Test {
             executors,
             address(this)
         );
+        vm.label(address(timelock), "TimelockController");
 
-        governor = new SummerEarnGovernor(
+        governor = new SummerGovernor(
             IVotes(address(token)),
             timelock,
             VOTING_DELAY,
@@ -68,6 +74,7 @@ contract SummerEarnGovernorTest is Test {
             PROPOSAL_THRESHOLD,
             QUORUM_FRACTION
         );
+        vm.label(address(governor), "SummerGovernor");
 
         timelock.grantRole(timelock.PROPOSER_ROLE(), address(governor));
         timelock.grantRole(timelock.CANCELLER_ROLE(), address(governor));
@@ -77,7 +84,7 @@ contract SummerEarnGovernorTest is Test {
     }
 
     function test_InitialSetup() public view {
-        assertEq(governor.name(), "SummerEarnGovernor");
+        assertEq(governor.name(), "SummerGovernor");
         assertEq(governor.votingDelay(), VOTING_DELAY);
         assertEq(governor.votingPeriod(), VOTING_PERIOD);
         assertEq(governor.proposalThreshold(), PROPOSAL_THRESHOLD);
@@ -85,12 +92,19 @@ contract SummerEarnGovernorTest is Test {
     }
 
     function test_ProposalCreation() public {
+        deal(address(token), alice, governor.proposalThreshold());
+        vm.roll(block.number + governor.votingDelay() + 1);
+
         vm.prank(alice);
         (uint256 proposalId, ) = createProposal();
+
         assertGt(proposalId, 0);
     }
 
     function test_Voting() public {
+        deal(address(token), alice, governor.proposalThreshold());
+        vm.roll(block.number + governor.votingDelay() + 1);
+
         vm.prank(alice);
         (uint256 proposalId, ) = createProposal();
 
@@ -105,6 +119,10 @@ contract SummerEarnGovernorTest is Test {
     }
 
     function test_ProposalExecution() public {
+        deal(address(token), address(timelock), 100);
+        deal(address(token), alice, governor.proposalThreshold());
+        vm.roll(block.number + governor.votingDelay() + 1);
+
         vm.prank(alice);
         (
             address[] memory targets,
@@ -148,38 +166,156 @@ contract SummerEarnGovernorTest is Test {
     }
 
     function test_Whitelisting() public {
-        vm.prank(address(timelock));
-        governor.setWhitelistAccountExpiration(
-            charlie,
-            block.timestamp + 1 days
+        address account = address(0x03);
+        uint256 expiration = block.timestamp + 10 days;
+
+        uint256 proposalThreshold = governor.proposalThreshold();
+
+        vm.prank(address(token));
+        deal(address(token), alice, proposalThreshold);
+
+        vm.startPrank(alice);
+        token.delegate(alice);
+        vm.roll(block.number + 1);
+        vm.stopPrank();
+
+        address[] memory targets = new address[](1);
+        targets[0] = address(governor);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSelector(
+            SummerGovernor.setWhitelistAccountExpiration.selector,
+            account,
+            expiration
         );
+        string memory description = "Whitelist account proposal";
 
-        assertTrue(governor.isWhitelisted(charlie));
-
-        vm.prank(charlie);
-        (uint256 proposalId, ) = createProposal();
-        assertGt(proposalId, 0);
-    }
-
-    function test_ProposalCancellation() public {
         vm.prank(alice);
-        (uint256 proposalId, ) = createProposal();
-
-        vm.prank(address(timelock));
-        governor.setWhitelistGuardian(bob);
-
-        vm.prank(bob);
-        (
-            address[] memory targets,
-            uint256[] memory values,
-            bytes[] memory calldatas,
-            string memory description
-        ) = createProposalParams();
-        governor.cancel(
+        uint256 proposalId = governor.propose(
             targets,
             values,
             calldatas,
-            hashDescription(description)
+            description
+        );
+
+        uint256 votingDelay = governor.votingDelay();
+        vm.warp(block.timestamp + votingDelay + 1);
+        vm.roll(block.number + votingDelay + 1);
+
+        vm.prank(alice);
+        governor.castVote(proposalId, 1);
+
+        uint256 votingPeriod = governor.votingPeriod();
+        vm.warp(block.timestamp + votingPeriod + 1);
+        vm.roll(block.number + votingPeriod + 1);
+
+        governor.queue(
+            targets,
+            values,
+            calldatas,
+            keccak256(bytes(description))
+        );
+
+        uint256 timelockDelay = timelock.getMinDelay();
+        vm.warp(block.timestamp + timelockDelay + 1);
+
+        governor.execute(
+            targets,
+            values,
+            calldatas,
+            keccak256(bytes(description))
+        );
+
+        bool isWhitelisted = governor.isWhitelisted(account);
+        uint256 actualExpiration = governor.whitelistAccountExpirations(
+            account
+        );
+
+        assertTrue(isWhitelisted, "Account should be whitelisted");
+        assertEq(
+            actualExpiration,
+            expiration,
+            "Expiration timestamp should match"
+        );
+    }
+
+    function test_ProposalCancellation() public {
+        deal(address(token), alice, governor.proposalThreshold());
+
+        vm.startPrank(alice);
+        token.delegate(alice);
+        vm.roll(block.number + 1); // Move to next block to activate voting power
+
+        // Create proposal to set Bob as whitelist guardian
+        address[] memory targets = new address[](1);
+        targets[0] = address(governor);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSelector(
+            SummerGovernor.setWhitelistGuardian.selector,
+            bob
+        );
+        string memory description = "Set Bob as whitelist guardian";
+
+        uint256 guardianProposalId = governor.propose(
+            targets,
+            values,
+            calldatas,
+            description
+        );
+
+        vm.warp(block.timestamp + governor.votingDelay() + 1);
+        vm.roll(block.number + governor.votingDelay() + 1);
+
+        governor.castVote(guardianProposalId, 1);
+
+        vm.warp(block.timestamp + governor.votingPeriod() + 1);
+        vm.roll(block.number + governor.votingPeriod() + 1);
+
+        governor.queue(
+            targets,
+            values,
+            calldatas,
+            keccak256(bytes(description))
+        );
+
+        vm.warp(block.timestamp + timelock.getMinDelay() + 1);
+
+        governor.execute(
+            targets,
+            values,
+            calldatas,
+            keccak256(bytes(description))
+        );
+
+        vm.stopPrank();
+
+        // Now Bob is the whitelist guardian, create a proposal to cancel
+        vm.startPrank(alice);
+        (uint256 proposalId, ) = createProposal();
+        vm.stopPrank();
+
+        // Assert that Bob is indeed the whitelist guardian
+        assertEq(
+            governor.whitelistGuardian(),
+            bob,
+            "Bob should be the whitelist guardian"
+        );
+
+        vm.prank(bob);
+        (
+            address[] memory cancelTargets,
+            uint256[] memory cancelValues,
+            bytes[] memory cancelCalldatas,
+            string memory cancelDescription
+        ) = createProposalParams();
+        governor.cancel(
+            cancelTargets,
+            cancelValues,
+            cancelCalldatas,
+            hashDescription(cancelDescription)
         );
 
         assertEq(
