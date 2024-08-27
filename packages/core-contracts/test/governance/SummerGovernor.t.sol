07ca98d54ebdf1d68f2ac586fd16b9eda2d41dc3
James Tuckett <james@tuckett.me>
2024-08-27 14:24:09 +0100

    chore: document SummerGovernor tests

--- packages/core-contracts/test/governance/SummerGovernor.t.sol
+++ packages/core-contracts/test/governance/SummerGovernor.t.sol
@@ -7,20 +7,39 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
+/*
+ * @title MockERC20Votes
+ * @dev A mock ERC20 token with voting capabilities for testing purposes.
+ */
 contract MockERC20Votes is ERC20, ERC20Permit, ERC20Votes {
     constructor() ERC20("Mock Token", "MTK") ERC20Permit("Mock Token") {}
 
+    /*
+     * @dev Mints tokens to a specified address.
+     * @param to The address to mint tokens to.
+     * @param amount The amount of tokens to mint.
+     */
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 
-    // Override the nonces function to resolve the conflict
+    /*
+     * @dev Overrides the nonces function to resolve conflicts between ERC20Permit and Nonces.
+     * @param owner The address to check nonces for.
+     * @return The current nonce for the given address.
+     */
     function nonces(
         address owner
     ) public view override(ERC20Permit, Nonces) returns (uint256) {
-        return super.nonces(owner); // Or choose one of the base implementations explicitly
+        return super.nonces(owner);
     }
 
+    /*
+     * @dev Internal function to update token balances.
+     * @param from The address to transfer tokens from.
+     * @param to The address to transfer tokens to.
+     * @param amount The amount of tokens to transfer.
+     */
     function _update(
         address from,
         address to,
@@ -30,6 +49,10 @@ contract MockERC20Votes is ERC20, ERC20Permit, ERC20Votes {
     }
 }
 
+/*
+ * @title SummerGovernorTest
+ * @dev Test contract for SummerGovernor functionality.
+ */
 contract SummerGovernorTest is Test {
     SummerGovernor public governor;
     MockERC20Votes public token;
@@ -45,6 +68,9 @@ contract SummerGovernorTest is Test {
     uint256 public constant PROPOSAL_THRESHOLD = 10000e18;
     uint256 public constant QUORUM_FRACTION = 4;
 
+    /*
+     * @dev Sets up the test environment.
+     */
     function setUp() public {
         vm.label(alice, "Alice");
         vm.label(bob, "Bob");
@@ -83,6 +109,9 @@ contract SummerGovernorTest is Test {
         token.delegate(alice);
     }
 
+    /*
+     * @dev Tests the initial setup of the governor.
+     */
     function test_InitialSetup() public view {
         assertEq(governor.name(), "SummerGovernor");
         assertEq(governor.votingDelay(), VOTING_DELAY);
@@ -91,6 +120,9 @@ contract SummerGovernorTest is Test {
         assertEq(governor.quorumNumerator(), QUORUM_FRACTION);
     }
 
+    /*
+     * @dev Tests the proposal creation process.
+     */
     function test_ProposalCreation() public {
         deal(address(token), alice, governor.proposalThreshold());
         vm.roll(block.number + governor.votingDelay() + 1);
@@ -101,6 +133,9 @@ contract SummerGovernorTest is Test {
         assertGt(proposalId, 0);
     }
 
+    /*
+     * @dev Tests the voting process on a proposal.
+     */
     function test_Voting() public {
         deal(address(token), alice, governor.proposalThreshold());
         vm.roll(block.number + governor.votingDelay() + 1);
@@ -118,6 +153,16 @@ contract SummerGovernorTest is Test {
         assertEq(forVotes, INITIAL_SUPPLY);
     }
 
+    /*
+     * @dev Tests the full proposal execution flow.
+     * This test covers:
+     * 1. Setting up initial token balances
+     * 2. Creating a proposal
+     * 3. Voting on the proposal
+     * 4. Queueing the proposal
+     * 5. Executing the proposal
+     * 6. Verifying the result of the execution
+     */
     function test_ProposalExecution() public {
         deal(address(token), address(timelock), 100);
         deal(address(token), alice, governor.proposalThreshold());
@@ -165,6 +210,9 @@ contract SummerGovernorTest is Test {
         assertEq(token.balanceOf(bob), 100);
     }
 
+    /*
+     * @dev Tests the whitelisting process through a governance proposal.
+     */
     function test_Whitelisting() public {
         address account = address(0x03);
         uint256 expiration = block.timestamp + 10 days;
@@ -240,6 +288,9 @@ contract SummerGovernorTest is Test {
         );
     }
 
+    /*
+     * @dev Tests the proposal cancellation process.
+     */
     function test_ProposalCancellation() public {
         deal(address(token), alice, governor.proposalThreshold());
 
@@ -324,6 +375,11 @@ contract SummerGovernorTest is Test {
         );
     }
 
+    /*
+     * @dev Creates a proposal for testing purposes.
+     * @return proposalId The ID of the created proposal.
+     * @return descriptionHash The hash of the proposal description.
+     */
     function createProposal() internal returns (uint256, bytes32) {
         (
             address[] memory targets,
@@ -341,6 +397,13 @@ contract SummerGovernorTest is Test {
         return (proposalId, hashDescription(description));
     }
 
+    /*
+     * @dev Creates parameters for a proposal.
+     * @return targets The target addresses for the proposal.
+     * @return values The values to be sent with the proposal.
+     * @return calldatas The function call data for the proposal.
+     * @return description The description of the proposal.
+     */
     function createProposalParams()
         internal
         view
@@ -366,6 +429,11 @@ contract SummerGovernorTest is Test {
         return (targets, values, calldatas, description);
     }
 
+    /*
+     * @dev Hashes the description of a proposal.
+     * @param description The description to hash.
+     * @return The keccak256 hash of the description.
+     */
     function hashDescription(
         string memory description
     ) internal pure returns (bytes32) {
