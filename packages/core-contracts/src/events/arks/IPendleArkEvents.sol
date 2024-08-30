74b6962923e7dae1fa609c91f7f1a1c490bbcbc9
Halaprix <panicz.klos@gmail.com>
2024-08-30 16:21:40 +0200

    feat: pendle LP and PT arks (#85)
    * feat: initial version of pednle pt ark
    
    * chore: fix decimals
    
    * chore: refactor oracle check
    
    * chore: update calcs for disembark
    
    * feat: Pendle LP ark
    
    * chore: refactor PT ark
    
    * chore: further refactor of the code
    
    * chore: add more tests
    
    * chore: deprecate rate in pendle arks
    
    * Update Codecov badge to current branch
    
    * chore: pass pendle touter as constructor param
    
    * chore: update docs
    
    * chore: remove redundant constants
    
    * Update Codecov badge to current branch
    
    * feat: use `Percentage` for slippage calculations
    
    * chore: refactor, use `Percentage` use custom errors, create interface and error file for pendle arks
    
    * chore: add more docs
    
    * chore: refactor, add pre & post expiry methods, add tests
    
    * chore: add more tests
    
    * chore: add more tests
    
    * chore: refactor
    
    * chore: refactor rate fetchers
    
    * chore: reafactor PendleBaseArk
    
    * chore: rafactor
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ packages/core-contracts/src/events/arks/IPendleArkEvents.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Percentage} from "@summerfi/percentage-solidity/contracts/PercentageUtils.sol";
+
+/**
+ * @title IPendleArkEvents
+ * @notice Interface for events emitted by Pendle Ark contracts
+ * @dev This interface defines events related to market rollovers, slippage updates, and oracle duration changes
+ */
+interface IPendleArkEvents {
+    /**
+     * @notice Emitted when the Pendle market is rolled over to a new market
+     * @dev This event is triggered during the rollover process when the current market expires
+     * @param newMarket The address of the new Pendle market after rollover
+     */
+    event MarketRolledOver(address indexed newMarket);
+
+    /**
+     * @notice Emitted when the slippage tolerance is updated
+     * @dev This event is triggered when the governor changes the slippage settings
+     * @param newSlippagePercentage The new slippage tolerance represented as a Percentage
+     */
+    event SlippageUpdated(Percentage newSlippagePercentage);
+
+    /**
+     * @notice Emitted when the oracle duration is updated
+     * @dev This event is triggered when the governor changes the oracle duration settings
+     * @param newOracleDuration The new oracle duration in seconds
+     */
+    event OracleDurationUpdated(uint32 newOracleDuration);
+}
