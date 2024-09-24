eae4981e3555899bea2d34f00369b858d6a8dc83
halaprix <panicz.klos@gmail.com>
2024-09-24 14:59:47 +0200

    chore: add docs to arks, unify the layout

--- packages/core-contracts/src/contracts/arks/MetaMorphoArk.sol
+++ packages/core-contracts/src/contracts/arks/MetaMorphoArk.sol
@@ -4,13 +4,28 @@ pragma solidity 0.8.26;
 import "../Ark.sol";
 import {IMetaMorpho} from "metamorpho/interfaces/IMetaMorpho.sol";
 
+/**
+ * @title MorphoVaultArk
+ * @notice This contract manages a Morpho Vaulttoken strategy within the Ark system
+ */
 contract MetaMorphoArk is Ark {
     using SafeERC20 for IERC20;
 
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    /// @notice The Morpho Vault address
     IMetaMorpho public immutable metaMorpho;
-    uint256 lastUpdate;
-    uint256 lastPrice;
 
+    /*//////////////////////////////////////////////////////////////
+                                CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Constructor for MetaMorphoArk
+     * @param _metaMorpho Address of the Morpho Vault
+     * @param _params ArkParams struct containing initialization parameters
+     */
     constructor(address _metaMorpho, ArkParams memory _params) Ark(_params) {
         if (_metaMorpho == address(0)) {
             revert InvalidVaultAddress();
@@ -18,36 +33,41 @@ contract MetaMorphoArk is Ark {
         metaMorpho = IMetaMorpho(_metaMorpho);
     }
 
+    /*//////////////////////////////////////////////////////////////
+                                VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @inheritdoc IArk
+     */
     function totalAssets() public view override returns (uint256 assets) {
         return metaMorpho.convertToAssets(metaMorpho.balanceOf(address(this)));
     }
 
+    /*//////////////////////////////////////////////////////////////
+                        INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Boards into the MetaMorpho Vault
+     * @param amount The amount of tokens to board
+     */
     function _board(uint256 amount, bytes calldata) internal override {
         config.token.approve(address(metaMorpho), amount);
         metaMorpho.deposit(amount, address(this));
         this.poke();
     }
 
+    /**
+     * @notice Disembarks from the MetaMorpho Vault
+     * @param amount The amount of tokens to disembark
+     */
     function _disembark(uint256 amount, bytes calldata) internal override {
         metaMorpho.withdraw(amount, address(this), address(this));
         this.poke();
     }
 
-    function poke() public override {
-        if (block.timestamp == lastUpdate) {
-            emit ArkPokedTooSoon();
-            return;
-        }
-        uint256 currentPrice = metaMorpho.convertToAssets(Constants.WAD);
-        if (currentPrice == lastPrice) {
-            emit ArkPokedNoChange();
-            return;
-        }
-        lastPrice = currentPrice;
-        lastUpdate = block.timestamp;
-        emit ArkPoked(lastPrice, lastUpdate);
-    }
-
+    // todo implement
     function _harvest(
         bytes calldata
     )
@@ -55,6 +75,15 @@ contract MetaMorphoArk is Ark {
         override
         returns (address[] memory rewardTokens, uint256[] memory rewardAmounts)
     {}
-    function _validateBoardData(bytes calldata data) internal override {}
-    function _validateDisembarkData(bytes calldata data) internal override {}
+
+    /**
+     * @notice Validates the board data
+     * @dev MetaMorpho Ark does not require any validation for board data
+     */
+    function _validateBoardData(bytes calldata) internal override {}
+    /**
+     * @notice Validates the disembark data
+     * @dev MetaMorpho Ark does not require any validation for board or disembark data
+     */
+    function _validateDisembarkData(bytes calldata) internal override {}
 }
--- packages/core-contracts/src/interfaces/IArk.sol
+++ packages/core-contracts/src/interfaces/IArk.sol
@@ -66,11 +66,6 @@ interface IArk is IArkAccessManaged, IArkEvents, IArkErrors {
     function commander() external view returns (address);
 
     /**
-     * @notice Updates information about the Ark
-     */
-    function poke() external;
-
-    /**
      * @notice Triggers a harvest operation to collect rewards
      * @param additionalData Optional bytes that might be required by a specific protocol to harvest
      * @return rewardTokens The reward token addresses
