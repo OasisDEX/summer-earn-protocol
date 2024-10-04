6ddeefcf60760c602d9c872eed7396ac7c71ac95
James Tuckett <james@tuckett.me>
2024-10-04 10:32:13 +0100

    feat: cross-chain proposal executions (#109)
    * feat: make SummerToken cross-chain capable
    
    * fix: PR review points
    
    * refactor: governance contracts + LZ into their own package (#106)
    
    * refactor: governance contracts + LZ into their own package
    
    * chore: remove test profile from /core-contracts
    
    * chore: format:fix
    
    * chore: update prettier ignore
    
    * chore: remove HH ignition from gov-contracts directory
    
    * chore: remove commented code
    
    * Update Codecov badge to current branch
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>
    
    * fix: burn account voting units on teleport
    
    * test: clear LZ logic from auction tests
    
    * fix: gov contracts after voting decay integration
    
    * Update Codecov badge to current branch
    
    * feat: simply execute proposals immediately on target chain
    
    * fix: GovernorTests
    
    * test: add additional test to confirm voting power is burned on teleport
    
    * test: cross chain proposal transmission
    
    * Revert "test: cross chain proposal transmission"
    
    This reverts commit 38a68f187d73c88db3bded60561646a6c505df08.
    
    * test: cross-chain-execution-on-source
    
    * fix: voting decay tests
    
    * chore: remove unnecessary files
    
    * chore: add poke back to interface so project builds
    
    * chore: comment out poke event assertions in tests
    
    * Update Codecov badge to current branch
    
    * fix: event emission check on receive
    
    * test: add additional non-happy path tests
    
    * Update Codecov badge to current branch
    
    * chore: sorting
    
    * Update Codecov badge to current branch
    
    * fix: typo
    
    * chore: add `proposalChainId` and some docs (#117)
    
    * chore: add `proposalChainId` and some docs
    
    * Update Codecov badge to current branch
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>
    
    * Update Codecov badge to current branch
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: halaprix <panicz.klos@gmail.com>

--- packages/gov-contracts/src/interfaces/ISummerGovernor.sol
+++ packages/gov-contracts/src/interfaces/ISummerGovernor.sol
@@ -2,11 +2,11 @@
 pragma solidity 0.8.27;
 
 import "@openzeppelin/contracts/governance/IGovernor.sol";
-
+import {ISummerGovernorErrors} from "../errors/ISummerGovernorErrors.sol";
 /* @title ISummerGovernor Interface
  * @notice Interface for the SummerGovernor contract, extending OpenZeppelin's IGovernor
  */
-interface ISummerGovernor is IGovernor {
+interface ISummerGovernor is IGovernor, ISummerGovernorErrors {
     /* @notice Emitted when a whitelisted account's expiration is set
      * @param account The address of the whitelisted account
      * @param expiration The timestamp when the account's whitelist status expires
@@ -20,6 +20,88 @@ interface ISummerGovernor is IGovernor {
      * @param newGuardian The address of the new whitelist guardian
      */
     event WhitelistGuardianSet(address indexed newGuardian);
+
+    /* @notice Emitted when a proposal is sent cross-chain
+     * @param proposalId The ID of the proposal
+     * @param dstEid The destination endpoint ID
+     */
+    event ProposalSentCrossChain(
+        uint256 indexed proposalId,
+        uint32 indexed dstEid
+    );
+
+    /* @notice Emitted when a proposal is received cross-chain
+     * @param proposalId The ID of the proposal
+     * @param srcEid The source endpoint ID
+     */
+    event ProposalReceivedCrossChain(
+        uint256 indexed proposalId,
+        uint32 indexed srcEid
+    );
+
+    /**
+     * @dev Proposes a new governance action.
+     * @param targets The addresses of the contracts to call.
+     * @param values The ETH values to send with the calls.
+     * @param calldatas The call data for each contract call.
+     * @param description A description of the proposal.
+     * @return proposalId The ID of the newly created proposal.
+     */
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) external override(IGovernor) returns (uint256 proposalId);
+
+    /**
+     * @dev Executes a proposal. Only callable on the proposal chain.
+     * Crosschain proposals are executed using LayerZero. Check _lzReceive for the execution logic.
+     * @param targets The addresses of the contracts to call.
+     * @param values The ETH values to send with the calls.
+     * @param calldatas The call data for each contract call.
+     * @param descriptionHash The hash of the proposal description.
+     * @return proposalId The ID of the executed proposal.
+     */
+    function execute(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) external payable override(IGovernor) returns (uint256 proposalId);
+
+    /**
+     * @dev Cancels an existing proposal.
+     * @param targets The addresses of the contracts to call.
+     * @param values The ETH values to send with the calls.
+     * @param calldatas The call data for each contract call.
+     * @param descriptionHash The hash of the proposal description.
+     * @return proposalId The ID of the cancelled proposal.
+     */
+    function cancel(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) external override(IGovernor) returns (uint256 proposalId);
+
+    /**
+     * @dev Sends a proposal to another chain for execution.
+     * @param _dstEid The destination Endpoint ID.
+     * @param _dstTargets The target addresses for the proposal.
+     * @param _dstValues The values for the proposal.
+     * @param _dstCalldatas The calldata for the proposal.
+     * @param _dstDescriptionHash The description hash for the proposal.
+     * @param _options Message execution options.
+     */
+    function sendProposalToTargetChain(
+        uint32 _dstEid,
+        address[] memory _dstTargets,
+        uint256[] memory _dstValues,
+        bytes[] memory _dstCalldatas,
+        bytes32 _dstDescriptionHash,
+        bytes calldata _options
+    ) external;
 
     /* @notice Checks if an account is whitelisted
      * @param account The address to check
@@ -40,4 +122,25 @@ interface ISummerGovernor is IGovernor {
      * @param _whitelistGuardian The address of the new whitelist guardian
      */
     function setWhitelistGuardian(address _whitelistGuardian) external;
+
+    /* @notice Sets a trusted remote for a specific chain ID
+     * @param _chainId The chain ID to set the trusted remote for
+     * @param _trustedRemote The address of the trusted remote on the specified chain
+     */
+    function setTrustedRemote(uint32 _chainId, address _trustedRemote) external;
+
+    /**
+     * @dev Gets the expiration time for a whitelisted account.
+     * @param account The address to check.
+     * @return The expiration timestamp for the account's whitelist status.
+     */
+    function getWhitelistAccountExpiration(
+        address account
+    ) external view returns (uint256);
+
+    /**
+     * @dev Gets the current whitelist guardian address.
+     * @return The address of the current whitelist guardian.
+     */
+    function getWhitelistGuardian() external view returns (address);
 }
