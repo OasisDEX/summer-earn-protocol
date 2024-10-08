6c8ffbf8cd91d6d389a9e20ef071d299845653eb
halaprix <panicz.klos@gmail.com>
2024-10-08 13:16:49 +0200

    chore: arbitrum deployment

--- tt.sh
+++ tt.sh
@@ -8,15 +8,6 @@ submodules=(
     "packages/core-contracts/lib/morpho-blue"
     "packages/core-contracts/lib/pendle-core-v2-public"
     "packages/core-contracts/lib/openzeppelin-contracts-upgradeable"
-    "packages/dutch-auction/lib/openzeppelin-contracts"
-    "packages/dutch-auction/lib/forge-std"
-    "packages/dutch-auction/lib/prb-math"
-    "packages/voting-decay/lib/openzeppelin-contracts"
-    "packages/voting-decay/lib/forge-std"
-    "packages/voting-decay/lib/prb-math"
-    "packages/gov-contracts/lib/forge-std"
-    "packages/gov-contracts/lib/openzeppelin-contracts-upgradeable"
-    "packages/gov-contracts/lib/prb-math"
 )
 
 for submodule in "${submodules[@]}"
