594386d87fe1887c71766165cc95ffd982c257c1
halaprix <panicz.klos@gmail.com>
2024-10-15 16:04:22 +0200

    chore: update apr calculations

--- packages/summer-earn-protocol-subgraph/src/mappings/ark.ts
+++ packages/summer-earn-protocol-subgraph/src/mappings/ark.ts
@@ -1,3 +1,23 @@
-import { Boarded } from '../../generated/templates/FleetCommanderTemplate/Ark'
+import { Ark as ArkContract } from '../../generated/HarborCommand/Ark'
+import { Boarded, Disembarked } from '../../generated/templates/FleetCommanderTemplate/Ark'
+import { getOrCreateArk } from '../common/initializers'
+import { handleBoard, handleDisembark } from './entities/ark'
 
-export function handleBoarded(event: Boarded): void {}
+export function handleBoarded(event: Boarded): void {
+  const arkContract = ArkContract.bind(event.address)
+  const vaultAddress = arkContract.commander()
+  const ark = getOrCreateArk(vaultAddress, event.address, event.block)
+
+  if (ark) {
+    handleBoard(event.params.amount, ark)
+  }
+}
+
+export function handleDisembarked(event: Disembarked): void {
+  const arkContract = ArkContract.bind(event.address)
+  const vaultAddress = arkContract.commander()
+  const ark = getOrCreateArk(vaultAddress, event.address, event.block)
+  if (ark) {
+    handleDisembark(event.params.amount, ark)
+  }
+}
