9027c90fdcf0884e1b29efe62a1046ad0069483b
James Tuckett <james@tuckett.me>
2024-10-23 13:07:56 +0100

    refactor: add role granting logic to gov.ts module and create proposal testing scripts

--- packages/deployment/ignition/modules/gov.ts
+++ packages/deployment/ignition/modules/gov.ts
@@ -1,10 +1,16 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
+import { keccak256, toBytes } from 'viem'
 import { ADDRESS_ZERO } from '../../scripts/common/constants'
 
 enum DecayType {
   Linear,
   Exponential,
 }
+
+const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000'
+const PROPOSER_ROLE = keccak256(toBytes('PROPOSER_ROLE'))
+const EXECUTOR_ROLE = keccak256(toBytes('EXECUTOR_ROLE'))
+const CANCELLER_ROLE = keccak256(toBytes('CANCELLER_ROLE'))
 
 export const GovModule = buildModule('GovModule', (m) => {
   const deployer = m.getAccount(0)
@@ -26,11 +32,13 @@ export const GovModule = buildModule('GovModule', (m) => {
    * - `executors`: accounts to be granted executor role
    * - `admin`: optional account to be granted admin role; disable with zero address
    */
+  const MIN_DELAY = 86400
+  const TEMP_MIN_DELAY_DURING_TESTING = MIN_DELAY / 24
   const timelock = m.contract('TimelockController', [
-    86400,
+    TEMP_MIN_DELAY_DURING_TESTING,
     [deployer],
     [ADDRESS_ZERO],
-    ADDRESS_ZERO,
+    deployer,
   ])
 
   /*
@@ -65,6 +73,16 @@ export const GovModule = buildModule('GovModule', (m) => {
 
   // Deploy SummerGovernor contract
   const summerGovernor = m.contract('SummerGovernor', [summerGovernorDeployParams])
+
+  m.call(timelock, 'grantRole', [PROPOSER_ROLE, summerGovernor.value])
+  m.call(timelock, 'grantRole', [CANCELLER_ROLE, summerGovernor.value])
+  m.call(timelock, 'grantRole', [EXECUTOR_ROLE, summerGovernor.value])
+
+  // Grant DEFAULT_ADMIN_ROLE back to the TimelockController itself
+  m.call(timelock, 'grantRole', [DEFAULT_ADMIN_ROLE, timelock.value])
+
+  // Revoke DEFAULT_ADMIN_ROLE from the deployer
+  m.call(timelock, 'revokeRole', [DEFAULT_ADMIN_ROLE, deployer])
 
   return {
     summerGovernor,
