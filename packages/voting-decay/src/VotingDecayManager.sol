d85aa5d580b75ae8b3762d2956e4351e4e1f0576
James Tuckett <james@tuckett.me>
2024-11-04 12:55:25 +0000

    chore: remove console from decay manager

--- packages/voting-decay/src/VotingDecayManager.sol
+++ packages/voting-decay/src/VotingDecayManager.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.28;
 
 import {VotingDecayLibrary} from "./VotingDecayLibrary.sol";
 import {IVotingDecayManager} from "./IVotingDecayManager.sol";
-import {console} from "forge-std/console.sol";
+
 /**
  * @title VotingDecayManager
  * @notice Manages voting power decay for accounts in a governance system
