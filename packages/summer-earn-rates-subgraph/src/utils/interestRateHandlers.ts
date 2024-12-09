95a8fc2df80705dfa03c1d45cafe0659365e3655
halaprix <panicz.klos@gmail.com>
2024-12-09 17:20:48 +0100

    feat: add daily and hourly averages

--- packages/summer-earn-rates-subgraph/src/utils/interestRateHandlers.ts
+++ packages/summer-earn-rates-subgraph/src/utils/interestRateHandlers.ts
@@ -1,5 +1,5 @@
-import { BigDecimal, ByteArray, crypto, ethereum } from '@graphprotocol/graph-ts'
-import { EMA, InterestRate } from '../../generated/schema'
+import { BigDecimal, BigInt, ByteArray, crypto, ethereum } from '@graphprotocol/graph-ts'
+import { DailyInterestRate, HourlyInterestRate, InterestRate } from '../../generated/schema'
 import { BigDecimalConstants } from '../constants/common'
 import { Product } from '../models/Product'
 
@@ -9,13 +9,30 @@ export function handleInterestRate(
   product: Product,
 ): void {
   const rate = product.getRate(block.timestamp, block.number)
-  updateEMA(block, protocolName, product, rate)
   const interestRate = new InterestRate(
     protocolName +
       product.token.id.toHexString() +
       block.number.toString() +
       crypto.keccak256(ByteArray.fromUTF8(product.name)).toHexString(),
   )
+  const dayTimestamp = block.timestamp.div(BigInt.fromI32(86400)).times(BigInt.fromI32(86400))
+
+  const dailyRateId =
+    protocolName +
+    product.token.id.toHexString() +
+    crypto.keccak256(ByteArray.fromUTF8(product.name)).toHexString() +
+    dayTimestamp.toString()
+
+  const hourTimestamp = block.timestamp.div(BigInt.fromI32(3600)).times(BigInt.fromI32(3600))
+
+  const hourlyRateId =
+    protocolName +
+    product.token.id.toHexString() +
+    crypto.keccak256(ByteArray.fromUTF8(product.name)).toHexString() +
+    hourTimestamp.toString()
+
+  interestRate.dailyRateId = dailyRateId
+  interestRate.hourlyRateId = hourlyRateId
   interestRate.blockNumber = block.number
   interestRate.rate = rate
   interestRate.timestamp = block.timestamp
@@ -24,54 +41,64 @@ export function handleInterestRate(
   interestRate.token = product.token.id
   interestRate.productId = product.name
   interestRate.save()
-}
 
-export function updateEMA(
+  updateDailyAverage(block, protocolName, product, rate, dayTimestamp, dailyRateId)
+  updateHourlyAverage(block, protocolName, product, rate, hourTimestamp, hourlyRateId)
+}
+function updateDailyAverage(
   block: ethereum.Block,
   protocolName: string,
   product: Product,
   newRate: BigDecimal,
+  dayTimestamp: BigInt,
+  dailyRateId: string,
 ): void {
-  const emaId =
-    protocolName +
-    product.token.id.toHexString() +
-    crypto.keccak256(ByteArray.fromUTF8(product.name)).toHexString() +
-    'EMA'
-
-  let ema = EMA.load(emaId)
-  if (ema === null) {
-    ema = new EMA(emaId)
-    ema.value = newRate
-    ema.lastUpdateTimestamp = block.timestamp
-    ema.protocol = protocolName
-    ema.token = product.token.id
-    ema.productId = product.name
-  } else {
-    if (newRate.equals(BigDecimalConstants.ZERO)) {
-      ema.lastUpdateTimestamp = block.timestamp
-      ema.save()
-      return
-    }
-    const timeDiff = block.timestamp.minus(ema.lastUpdateTimestamp).toI32()
-    const N = 21600 // 6 hours in seconds
-    const alpha = BigDecimal.fromString('1').minus(
-      BigDecimal.fromString('1').div(
-        BigDecimal.fromString('1').plus(BigDecimal.fromString(N.toString())),
-      ),
-    )
-    const weight = BigDecimal.fromString('1').minus(alpha)
-
-    // Calculate the decay factor
-    let decayFactor = BigDecimal.fromString('1')
-    for (let i = 0; i < timeDiff; i++) {
-      decayFactor = decayFactor.times(weight)
-    }
-
-    // Update EMA
-    ema.value = ema.value
-      .times(decayFactor)
-      .plus(newRate.times(BigDecimal.fromString('1').minus(decayFactor)))
-    ema.lastUpdateTimestamp = block.timestamp
+  let dailyRate = DailyInterestRate.load(dailyRateId)
+  if (dailyRate === null) {
+    dailyRate = new DailyInterestRate(dailyRateId)
+    dailyRate.date = dayTimestamp
+    dailyRate.sumRates = BigDecimalConstants.ZERO
+    dailyRate.updateCount = BigInt.fromI32(0)
+    dailyRate.averageRate = BigDecimalConstants.ZERO
+    dailyRate.protocol = protocolName
+    dailyRate.token = product.token.id
+    dailyRate.productId = product.name
   }
-  ema.save()
+
+  dailyRate.sumRates = dailyRate.sumRates.plus(newRate)
+  dailyRate.updateCount = dailyRate.updateCount.plus(BigInt.fromI32(1))
+  dailyRate.averageRate = dailyRate.sumRates.div(
+    BigDecimal.fromString(dailyRate.updateCount.toString()),
+  )
+
+  dailyRate.save()
+}
+
+function updateHourlyAverage(
+  block: ethereum.Block,
+  protocolName: string,
+  product: Product,
+  newRate: BigDecimal,
+  hourTimestamp: BigInt,
+  hourlyRateId: string,
+): void {
+  let hourlyRate = HourlyInterestRate.load(hourlyRateId)
+  if (hourlyRate === null) {
+    hourlyRate = new HourlyInterestRate(hourlyRateId)
+    hourlyRate.date = hourTimestamp
+    hourlyRate.sumRates = BigDecimalConstants.ZERO
+    hourlyRate.updateCount = BigInt.fromI32(0)
+    hourlyRate.averageRate = BigDecimalConstants.ZERO
+    hourlyRate.protocol = protocolName
+    hourlyRate.token = product.token.id
+    hourlyRate.productId = product.name
+  }
+
+  hourlyRate.sumRates = hourlyRate.sumRates.plus(newRate)
+  hourlyRate.updateCount = hourlyRate.updateCount.plus(BigInt.fromI32(1))
+  hourlyRate.averageRate = hourlyRate.sumRates.div(
+    BigDecimal.fromString(hourlyRate.updateCount.toString()),
+  )
+
+  hourlyRate.save()
 }
