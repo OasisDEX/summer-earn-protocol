0deec6d1817810d9824083710fc0682fd51df359
James Tuckett <james@tuckett.me>
2025-01-10 15:28:52 +0000

    refactor: enhance ICometRewards interface with additional claim functionality
    - Added new `claimTo` function to allow claiming rewards to a specified address.
    - Updated documentation for `rewardConfig` and `claim` functions to improve clarity and usability.
    - Enhanced the interface to support more flexible reward claiming options, aligning with recent changes in reward handling.

--- packages/core-contracts/src/interfaces/compound-v3/ICometRewards.sol
+++ packages/core-contracts/src/interfaces/compound-v3/ICometRewards.sol
@@ -8,10 +8,26 @@ interface ICometRewards {
         bool shouldUpscale;
     }
 
+    /// @notice Reward token address per Comet instance
     function rewardConfig(
         address comet
     ) external view returns (RewardConfig memory);
+
+    /**
+     * @notice Claim rewards of token type from a comet instance to owner address
+     * @param comet The protocol instance
+     * @param src The owner to claim for
+     * @param shouldAccrue Whether or not to call accrue first
+     */
     function claim(address comet, address src, bool shouldAccrue) external;
+
+    /**
+     * @notice Claim rewards of token type from a comet instance to a target address
+     * @param comet The protocol instance
+     * @param src The owner to claim for
+     * @param to The address to receive the rewards
+     * @param shouldAccrue Whether or not to call accrue first
+     */
     function claimTo(
         address comet,
         address src,
