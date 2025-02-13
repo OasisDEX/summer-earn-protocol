7aac4cfc0330c26cbc3a5af6835b379274edcad3
halaprix <panicz.klos@gmail.com>
2025-02-13 14:00:35 +0100

    chore: add product ID to ark

--- packages/summer-earn-protocol-subgraph/src/utils/ark.ts
+++ packages/summer-earn-protocol-subgraph/src/utils/ark.ts
@@ -1,5 +1,5 @@
-import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
-import { Token } from '../../generated/schema'
+import { Address, BigInt, ethereum, json, JSONValue, JSONValueKind } from '@graphprotocol/graph-ts'
+import { Ark, Token } from '../../generated/schema'
 import { Ark as ArkContract } from '../../generated/templates/FleetCommanderTemplate/Ark'
 import * as constants from '../common/constants'
 import { getOrCreateArk } from '../common/initializers'
@@ -30,4 +30,59 @@ export function getArkDetails(
   )
 
   return arkDetails
+}
+
+export function getArkProductId(ark: Ark): string | null {
+  if (!ark.details || ark.name == 'BufferArk') {
+    return null
+  }
+
+  const details = ark.details!.toString()
+  let jsonValue = json.fromString(details)
+  const jsonData = jsonValue.toObject()
+
+  if (
+    !jsonData ||
+    !jsonData.isSet('pool') ||
+    !jsonData.isSet('protocol') ||
+    !jsonData.isSet('chainId')
+  ) {
+    return null
+  }
+
+  const protocolValue = jsonData.get('protocol')
+  const poolValue = jsonData.get('pool')
+  const chainIdValue = jsonData.get('chainId')
+
+  // Check if all values are strings
+  if (!protocolValue || !poolValue || !chainIdValue) {
+    return null
+  }
+
+  let protocol = decodeValue(protocolValue)
+  const pool = decodeValue(poolValue)
+  const chainId = decodeValue(chainIdValue)
+
+  if (protocol === 'gearbox') {
+    protocol = 'Gearbox'
+  }
+  if (protocol === 'fluid') {
+    protocol = 'Fluid'
+  }
+
+  const assetAddress = ark.inputToken.toLowerCase()
+  const poolAddress = pool.toLowerCase()
+  const normalizedChainId = chainId.toLowerCase()
+
+  return `${protocol}-${assetAddress}-${poolAddress}-${normalizedChainId}`
+}
+
+export function decodeValue(value: JSONValue): string {
+  if (value.kind == JSONValueKind.STRING) {
+    return value.toString()
+  }
+  if (value.kind == JSONValueKind.NUMBER) {
+    return value.toBigInt().toString()
+  }
+  return ''
 }
