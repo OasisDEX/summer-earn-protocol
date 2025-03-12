068079cc2841f0bf4bd9d196b3534e3fb723203f
James Tuckett <james@tuckett.me>
2025-03-12 10:41:29 +0000

    feat: Add Sonic network support to Hardhat deployment configuration

--- packages/deployment/hardhat.config.ts
+++ packages/deployment/hardhat.config.ts
@@ -16,9 +16,11 @@ if (!process.env.API_KEY_ARBISCAN) {
 if (!process.env.API_KEY_BASESCAN) {
   throw new Error('Please set your process.env.API_KEY_BASESCAN in a .env file')
 }
-
 if (!process.env.API_KEY_ETHERSCAN) {
   throw new Error('Please set your process.env.API_KEY_ETHERSCAN in a .env file')
+}
+if (!process.env.API_KEY_SONICSCAN) {
+  throw new Error('Please set your process.env.API_KEY_SONICSCAN in a .env file')
 }
 
 const config: HardhatUserConfig = {
@@ -83,6 +85,11 @@ const config: HardhatUserConfig = {
       url: `${process.env.BASE_RPC_URL}`,
       accounts: [`0x${process.env.DEPLOYER_PRIV_KEY}`],
       chainId: 8453,
+    },
+    sonic: {
+      url: `${process.env.SONIC_RPC_URL}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIV_KEY}`],
+      chainId: 146,
     },
 
     // testnets
