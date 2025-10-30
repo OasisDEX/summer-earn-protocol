096a9bf19787baac6c56f6863b0ee7d50914c2c7
Roberto Cano <3525807+robercano@users.noreply.github.com>
2025-10-30 16:33:36 +0100

    fix: bring comments coverage up to spec

--- packages/chain-bridge/src/helpers/LayerZeroMessagingHelper.sol
+++ packages/chain-bridge/src/helpers/LayerZeroMessagingHelper.sol
@@ -6,14 +6,18 @@ import {IBridgeAdapter} from "../interfaces/IBridgeAdapter.sol";
 
 /**
  * @title LayerZeroMessagingHelper
+ *
  * @notice Helper library for LayerZero-specific messaging utilities
+ *
  * @dev Provides utilities for creating message parameters, validating fees, and handling LayerZero-specific operations
  */
 library LayerZeroMessagingHelper {
     /**
      * @notice Creates RelayedMessageParams from ExecuteSendMessageParams
+     *
      * @param params ExecuteSendMessageParams from the bridge operation
      * @param operationId The operation ID for this transaction
+     *
      * @return RelayedMessageParams struct ready for encoding
      */
     function createRelayedMessageParams(
@@ -32,6 +36,7 @@ library LayerZeroMessagingHelper {
 
     /**
      * @notice Validates that sufficient msg.value was provided for the operation
+     *
      * @param options Bridge options containing msgValue requirement
      * @param msgValue The actual msg.value sent with the transaction
      */
@@ -39,6 +44,7 @@ library LayerZeroMessagingHelper {
         BridgeTypes.BridgeOptions calldata options,
         uint256 msgValue
     ) internal pure {
+        // Sent fee must meet or exceed required msgValue
         if (options.msgValue > 0 && msgValue < options.msgValue) {
             revert IBridgeAdapter.InsufficientMsgValue(
                 options.msgValue,
@@ -49,6 +55,7 @@ library LayerZeroMessagingHelper {
 
     /**
      * @notice Creates dummy message parameters for fee estimation
+     *
      * @return params Dummy RelayedMessageParams for estimation purposes
      */
     function createDummyMessageParams()
