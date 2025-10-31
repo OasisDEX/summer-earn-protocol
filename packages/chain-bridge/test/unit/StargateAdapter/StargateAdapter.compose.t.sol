187b77b8f3e4a04c6bd0a0ac99f68126bc7a85a8
Roberto Cano <3525807+robercano@users.noreply.github.com>
2025-10-31 15:54:08 +0100

    chore: remove unused variable

--- packages/chain-bridge/test/unit/StargateAdapter/StargateAdapter.compose.t.sol
+++ packages/chain-bridge/test/unit/StargateAdapter/StargateAdapter.compose.t.sol
@@ -18,8 +18,6 @@ contract StargateAdapterComposeTest is
     MockFleetProxy public fleetProxyA;
     MockFleetProxy public fleetProxyB;
 
-    bytes public testMessage;
-
     function setUp() public override {
         super.setUp();
 
