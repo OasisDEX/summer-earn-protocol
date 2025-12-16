095713901dc6cd10335efe30297d36bf176a89cf
halaprix <panicz.klos@gmail.com>
2025-12-16 13:59:36 +0100

    refactor: reorganize MidasArk contract structure and enhance functionality
    - Updated import statements for clarity and added necessary interfaces.
    - Refactored state variables into distinct sections for better organization.
    - Improved asset calculation methods by introducing internal functions for shares to assets conversion.
    - Enhanced withdrawal logic to streamline request handling and ensure accurate asset retrieval.
    - Removed redundant code and comments to improve readability and maintainability.

--- packages/core-contracts/src/contracts/arks/MidasArk.sol
+++ packages/core-contracts/src/contracts/arks/MidasArk.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import "../ArkWithWithdrawalRequest.sol";
-import {IDepositVault} from "../../interfaces/midas/IDepositVault.sol";
-import {IRedemptionVault} from "../../interfaces/midas/IRedemptionVault.sol";
-import {IMidasOracle} from "../../interfaces/midas/IMidasOracle.sol";
-import {Request} from "../../interfaces/midas/IRedemptionVault.sol";
-import {IMToken} from "../../interfaces/midas/IMToken.sol";
-import {Constants} from "@summerfi/constants/Constants.sol";
-import {TokenConfig} from "../../interfaces/midas/IManageableVault.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
-import {RequestStatus} from "../../interfaces/midas/IRedemptionVault.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Constants} from "@summerfi/constants/Constants.sol";
+
+import {ArkWithWithdrawalRequest, ArkParams, IArk, Ark, IArkWithWithdrawalRequest} from "../ArkWithWithdrawalRequest.sol";
+import {IDepositVault} from "../../interfaces/midas/IDepositVault.sol";
+import {IRedemptionVault, Request, RequestStatus} from "../../interfaces/midas/IRedemptionVault.sol";
+import {IMidasOracle} from "../../interfaces/midas/IMidasOracle.sol";
+import {IMToken} from "../../interfaces/midas/IMToken.sol";
+import {TokenConfig} from "../../interfaces/midas/IManageableVault.sol";
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 error InvalidIssuanceVault();
 error InvalidRedemptionVault();
 error InvalidOracle();
@@ -31,36 +34,50 @@ contract MidasArk is ArkWithWithdrawalRequest {
     using SafeERC20 for IRedemptionVault;
 
     /*//////////////////////////////////////////////////////////////
-                            STATE VARIABLES
+                           CONSTANTS
     //////////////////////////////////////////////////////////////*/
-
-    IDepositVault public immutable issuanceVault;
-    IMToken public immutable mToken;
-    IRedemptionVault public immutable redemptionVault;
-    IMidasOracle public immutable oracle;
-    /// @notice The request ID for the redemption
-    uint256 public withdrawalRequestId;
 
     /**
      * @notice 100 percent with base 100
      * @dev for example, 10% will be (10 * 100)%
      */
     uint256 public constant ONE_HUNDRED_PERCENT = 100 * 100;
+
     /**
      * @notice Default slippage for the swap
      * @dev 50 is 50% (50/10000)
      */
     uint256 public constant DEFAULT_SLIPPAGE = 50;
+
+    /**
+     * @notice Rounding offset for Midas calculations
+     */
+    uint256 public constant MIDAS_ROUNDING_OFFSET = 1;
+
+    /*//////////////////////////////////////////////////////////////
+                           IMMUTABLE STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    IDepositVault public immutable issuanceVault;
+    IMToken public immutable mToken;
+    IRedemptionVault public immutable redemptionVault;
+    IMidasOracle public immutable oracle;
+
     /**
      * @notice The conversion factor from the underlying asset to the mToken
      * @dev e.g. 1e12 is the conversion factor for USDC (mTokens have 18 decimals)
      */
     uint256 public immutable TO_M_TOKEN_DECIMALS;
 
-    uint256 public constant MIDAS_ROUNDING_OFFSET = 1;
+    /*//////////////////////////////////////////////////////////////
+                           MUTABLE STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The request ID for the redemption
+    uint256 public withdrawalRequestId;
 
     /*//////////////////////////////////////////////////////////////
-                                CONSTRUCTOR
+                           CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
     /**
@@ -85,21 +102,25 @@ contract MidasArk is ArkWithWithdrawalRequest {
         mToken = IMToken(_mToken);
         oracle = IMidasOracle(issuanceVault.mTokenDataFeed());
 
+        // Verify mToken matches vaults
         IMToken issuanceVaultMToken = issuanceVault.mToken();
         if (address(issuanceVaultMToken) != _mToken)
             revert InvalidMTokenAddress();
+
         IMToken redemptionVaultMToken = redemptionVault.mToken();
         if (address(redemptionVaultMToken) != _mToken)
             revert InvalidMTokenAddress();
 
+        // Calculate conversion factor
         uint8 mTokenDecimals = mToken.decimals();
         uint8 assetDecimals = IERC20Metadata(address(config.asset)).decimals();
         if (mTokenDecimals < assetDecimals) revert InvalidMTokenDecimals();
+
         TO_M_TOKEN_DECIMALS = 10 ** (mTokenDecimals - assetDecimals);
     }
 
     /*//////////////////////////////////////////////////////////////
-                            PUBLIC FUNCTIONS
+                           PUBLIC VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
     /**
@@ -115,14 +136,13 @@ contract MidasArk is ArkWithWithdrawalRequest {
     {
         assets += _withdrawableTotalAssets();
         assets += assetsInWithdrawalQueue();
+
         // Add value of shares held by Ark using oracle price
         // Check both issuance and redemption mToken shares (they may use the same share token)
-        uint256 sharesFromIssuance = mToken.balanceOf(address(this));
-        if (sharesFromIssuance > 0) {
+        uint256 shares = mToken.balanceOf(address(this));
+        if (shares > 0) {
             uint256 price = oracle.getDataInBase18();
-            uint256 assetsFromShares = (sharesFromIssuance * price) /
-                (TO_M_TOKEN_DECIMALS * Constants.WAD);
-            assets += assetsFromShares;
+            assets += _sharesToAssets(shares, price);
         }
     }
 
@@ -130,22 +150,32 @@ contract MidasArk is ArkWithWithdrawalRequest {
      * @inheritdoc IArkWithWithdrawalRequest
      */
     function assetsInWithdrawalQueue() public view returns (uint256) {
-        if (withdrawalRequestId > 0) {
-            Request memory request = redemptionVault.redeemRequests(
-                withdrawalRequestId
-            );
-            if (request.status != RequestStatus.Pending) {
-                return 0;
-            }
-            // Use oracle to convert shares to assets
-            // todo: do we need to take into account the `tokenOutRate`?
-            return
-                (request.amountMToken * request.mTokenRate) /
-                (Constants.WAD * TO_M_TOKEN_DECIMALS);
-        } else {
+        if (withdrawalRequestId == 0) {
             return 0;
         }
+
+        Request memory request = redemptionVault.redeemRequests(
+            withdrawalRequestId
+        );
+        if (request.status != RequestStatus.Pending) {
+            return 0;
+        }
+
+        // TODO: check if we need to take into account the `tokenOutRate`
+        return _sharesToAssets(request.amountMToken, request.mTokenRate);
     }
+
+    /**
+     * @inheritdoc IArkWithWithdrawalRequest
+     * @notice Midas processes withdrawals automatically
+     */
+    function isWithdrawalClaimRequired() public pure returns (bool) {
+        return false;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL MUTABLE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /**
      * @notice Request redemption of shares from the Midas redemption mToken
@@ -160,25 +190,23 @@ contract MidasArk is ArkWithWithdrawalRequest {
                 revert WithdrawalAlreadyRequested();
             }
         }
-        uint256 amountIn18Decimals = 0;
-        uint256 shares = 0;
+
+        uint256 shares;
         if (amount == type(uint256).max) {
             // Get total shares from both vaults (they may use the same share token)
             shares = mToken.balanceOf(address(this));
         } else {
-            amountIn18Decimals = amount * TO_M_TOKEN_DECIMALS;
-            // Use oracle to convert assets to shares
-            uint256 price = oracle.getDataInBase18();
-            uint256 sharesFromAssetsIn18Decimals = (amountIn18Decimals *
-                Constants.WAD) / (price);
-            shares = sharesFromAssetsIn18Decimals;
+            shares = _assetsToShares(amount);
         }
+
         mToken.approve(address(redemptionVault), shares);
+
         uint256 requestId = redemptionVault.redeemRequest(
             address(config.asset),
             shares
         );
         withdrawalRequestId = requestId;
+
         emit WithdrawalRequested(amount, requestId);
     }
 
@@ -192,23 +220,14 @@ contract MidasArk is ArkWithWithdrawalRequest {
 
     /**
      * @inheritdoc IArkWithWithdrawalRequest
-     * @notice Midas processes withdrawals automatically
-     */
-    function isWithdrawalClaimRequired() public view returns (bool) {
-        return false;
-    }
-
-    /**
-     * @inheritdoc IArkWithWithdrawalRequest
      */
     function withdrawUsingSwap(
         uint256 amount,
         bytes calldata data
     ) external onlyKeeper nonReentrant {
-        // Use oracle to convert assets to shares
-        uint256 price = oracle.getDataInBase18();
-        uint256 shares = (amount * TO_M_TOKEN_DECIMALS * Constants.WAD) / price;
+        uint256 shares = _assetsToShares(amount);
         SwapData memory swapData = abi.decode(data, (SwapData));
+
         uint256 assetBought = _swap(
             address(address(mToken)),
             address(config.asset),
@@ -217,12 +236,13 @@ contract MidasArk is ArkWithWithdrawalRequest {
             _applySlippage(amount) - MIDAS_ROUNDING_OFFSET,
             swapData.swapCalldata
         );
+
         emit Disembarked(msg.sender, address(config.asset), amount);
         _boardToBufferArk(assetBought);
     }
 
     /*//////////////////////////////////////////////////////////////
-                            INTERNAL FUNCTIONS
+                           INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
     /**
@@ -241,15 +261,15 @@ contract MidasArk is ArkWithWithdrawalRequest {
     }
 
     function _board(uint256 amount, bytes calldata) internal override {
-        uint256 price = oracle.getDataInBase18();
         uint256 amountScaledTo18Decimals = amount * TO_M_TOKEN_DECIMALS;
-        uint256 minReceiveAmountIn18Decimals = (amountScaledTo18Decimals *
-            Constants.WAD) / price;
+        // Calculate minimum receive amount using the oracle price
+        uint256 minReceiveAmountInMTokenDecimals = _assetsToShares(amount);
+
         config.asset.forceApprove(address(issuanceVault), amount);
         issuanceVault.depositInstant(
             address(config.asset),
             amountScaledTo18Decimals,
-            minReceiveAmountIn18Decimals,
+            minReceiveAmountInMTokenDecimals,
             bytes32(0)
         );
     }
@@ -292,4 +312,32 @@ contract MidasArk is ArkWithWithdrawalRequest {
      * @param /// data Additional data to validate
      */
     function _validateDisembarkData(bytes calldata) internal pure override {}
+
+    /*//////////////////////////////////////////////////////////////
+                           PRIVATE HELPER FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Converts shares to assets using the provided price
+     * @param shares Amount of shares to convert
+     * @param price Current price from oracle in WAD
+     * @return assets Amount of assets equivalent to shares
+     */
+    function _sharesToAssets(
+        uint256 shares,
+        uint256 price
+    ) internal view returns (uint256) {
+        return (shares * price) / (TO_M_TOKEN_DECIMALS * Constants.WAD);
+    }
+
+    /**
+     * @notice Converts assets to shares using the current oracle price
+     * @param assets Amount of assets to convert
+     * @return shares Amount of shares equivalent to assets
+     */
+    function _assetsToShares(uint256 assets) internal view returns (uint256) {
+        uint256 price = oracle.getDataInBase18();
+        uint256 amountInMTokenDecimals = assets * TO_M_TOKEN_DECIMALS;
+        return (amountInMTokenDecimals * Constants.WAD) / price;
+    }
 }
