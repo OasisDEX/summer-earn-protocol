28f226da91523d79843605c8f4e47c5f71e3b1d9
Roberto Cano <3525807+robercano@users.noreply.github.com>
2026-02-06 16:47:31 +0100

    wip: added VaultWithReceipts

---
+++ packages/core-contracts/src/interfaces/rounds-vault/IVaultWithReceiptsErrors.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+/**
+    @notice Errors for the VaultWithReceipts contract
+
+    @author Roberto Cano <robercano>
+ */
+interface IVaultWithReceiptsErrors {
+    /// Emitted when trying to deposit an amount of assets that exceeds the max allowed for
+    /// the depositor
+    error MaxDepositExceeded(address depositor, uint256 amount, uint256 max);
+
+    /// Emitted when trying to redeem an amount of shares that exceeds the max allowed for
+    /// the redeemer by the owner of the shares
+    error MaxRedeemExceeded(
+        address redeemer,
+        address owner,
+        uint256 id,
+        uint256 amount,
+        uint256 max
+    );
+
+    /// Emitted when trying to redeem a batch of shares and the total amount exceeds the max allowed for
+    /// the redeemer by the owner of the shares
+    error MaxRedeemBatchExceeded(
+        address redeemer,
+        address owner,
+        uint256[] ids,
+        uint256 amount,
+        uint256 max
+    );
+
+    /// Emitted when trying to redeem an amount of shares and the caller is not the owner
+    /// or the amount that exceeds the max allowed by the owner for the redeemer
+    error CallerCannotRedeem(
+        address caller,
+        address owner,
+        uint256 id,
+        uint256 amount
+    );
+
+    /// Emitted when trying to redeem a batch of shares and the caller is not the owner
+    /// or the total amount exceeds the max allowed by the owner for the redeemer
+    error CallerCannotRedeemBatch(
+        address caller,
+        address owner,
+        uint256[] ids,
+        uint256[] amounts
+    );
+
+    /// Emitted when trying to redeem a batch of shares and the lengths of the ids and amounts arrays do not match
+    error BadRedeemBatchParameters(uint256 idsLength, uint256 amountsLength);
+}
