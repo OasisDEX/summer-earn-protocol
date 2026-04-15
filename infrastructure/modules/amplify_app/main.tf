1eaa7f67e4010d98b4e5a370138b37098f0aecb9
halaprix <6533433+halaprix@users.noreply.github.com>
2026-04-15 15:58:16 +0200

    chore: rename amplify role and revert to managed AdministratorAccess-Amplify policy attachment

--- infrastructure/modules/amplify_app/main.tf
+++ infrastructure/modules/amplify_app/main.tf
@@ -12,88 +12,16 @@ data "aws_iam_policy_document" "amplify_trust" {
 }
 
 resource "aws_iam_role" "this" {
-  name               = "${var.app_name}-amplify-service-role"
+  # Renaming role to force a new ARN and refresh Amplify's internal association
+  name               = "${var.app_name}-amplify-role-v1"
   assume_role_policy = data.aws_iam_policy_document.amplify_trust.json
 
   tags = var.tags
 }
 
-# Scoped Amplify permissions instead of AdministratorAccess-Amplify
-data "aws_iam_policy_document" "amplify_permissions" {
-  # Amplify service permissions
-  statement {
-    sid = "AmplifyManagement"
-    actions = [
-      "amplify:GetApp",
-      "amplify:GetBranch",
-      "amplify:CreateBranch",
-      "amplify:DeleteBranch",
-      "amplify:CreateDeployment",
-      "amplify:StartDeployment",
-      "amplify:StopDeployment",
-      "amplify:GetJob",
-      "amplify:ListJobs",
-      "amplify:StartJob",
-      "amplify:StopJob",
-    ]
-    resources = ["*"]
-  }
-
-  # CloudFront for hosting
-  statement {
-    sid = "CloudFrontHosting"
-    actions = [
-      "cloudfront:CreateInvalidation",
-      "cloudfront:GetDistribution",
-      "cloudfront:GetInvalidation",
-      "cloudfront:ListDistributions",
-      "cloudfront:ListInvalidations",
-      "cloudfront:UpdateDistribution",
-    ]
-    resources = ["*"]
-  }
-
-  # S3 for build artifacts and hosting
-  statement {
-    sid = "S3BuildArtifacts"
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject",
-      "s3:ListBucket",
-      "s3:GetBucketLocation",
-    ]
-    resources = ["*"]
-  }
-
-  # CloudWatch Logs for build logs
-  statement {
-    sid = "CloudWatchLogs"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:DescribeLogGroups",
-    ]
-    resources = ["*"]
-  }
-
-  # SSM for environment variable management
-  statement {
-    sid = "SSMParameters"
-    actions = [
-      "ssm:GetParameter",
-      "ssm:GetParameters",
-      "ssm:GetParametersByPath",
-    ]
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role_policy" "this" {
-  name   = "${var.app_name}-amplify-policy"
-  role   = aws_iam_role.this.id
-  policy = data.aws_iam_policy_document.amplify_permissions.json
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess-Amplify"
 }
 
 resource "aws_amplify_app" "this" {
