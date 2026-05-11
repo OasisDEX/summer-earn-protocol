ab3125d07530f930064cbcb981a0021bc54efcab
halaprix <6533433+halaprix@users.noreply.github.com>
2026-05-11 10:32:58 +0200

    feat: enable outputFileTracingRoot in next.config.js for correct monorepo path resolution

--- packages/summer-earn-gov-validator/next.config.js
+++ packages/summer-earn-gov-validator/next.config.js
@@ -7,9 +7,9 @@ const __dirname = path.dirname(__filename)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // experimental: {
-  //   outputFileTracingRoot: path.join(__dirname, '../../'),
-  // },
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 }
 
 export default nextConfig;
