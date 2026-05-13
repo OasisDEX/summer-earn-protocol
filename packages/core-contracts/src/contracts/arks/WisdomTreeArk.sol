b97f97b6b1f9f9d110ca9d760975d6c6b4b79f77
Roberto Cano <3525807+robercano@users.noreply.github.com>
2026-05-13 09:27:43 +0200

    fix(core-contracts): prevent sweep when WisdomTreeArk is frozen

--- packages/core-contracts/src/contracts/arks/WisdomTreeArk.sol
+++ packages/core-contracts/src/contracts/arks/WisdomTreeArk.sol
@@ -364,6 +364,7 @@ contract WisdomTreeArk is ArkWithWithdrawalRequest, ERC721Holder {
         public
         override
         onlyKeeper
+        onlyNotFrozen
         nonReentrant
         returns (address[] memory sweptTokens, uint256[] memory sweptAmounts)
     {
