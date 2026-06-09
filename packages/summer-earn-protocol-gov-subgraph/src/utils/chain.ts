acf33c1877376132eb0bde239895893a4bb70951
halaprix <6533433+halaprix@users.noreply.github.com>
2026-06-09 21:29:35 +0200

    feat: add HyperEVM support, update grafting configurations, and implement safety checks for subgraph map lookups

--- packages/summer-earn-protocol-gov-subgraph/src/utils/chain.ts
+++ packages/summer-earn-protocol-gov-subgraph/src/utils/chain.ts
@@ -4,12 +4,14 @@ dstEidToChainIdMap.set('30101', '1') // Ethereum Mainnet
 dstEidToChainIdMap.set('30110', '42161') // Arbitrum
 dstEidToChainIdMap.set('30184', '8453') // Base
 dstEidToChainIdMap.set('30332', '146') // Sonic
+dstEidToChainIdMap.set('30367', '999') // HyperEVM
 
 export const subgraphNetworkToChainIdMap = new Map<string, string>()
 subgraphNetworkToChainIdMap.set('mainnet', '1')
 subgraphNetworkToChainIdMap.set('arbitrum-one', '42161')
 subgraphNetworkToChainIdMap.set('base', '8453')
 subgraphNetworkToChainIdMap.set('sonic-mainnet', '146')
+subgraphNetworkToChainIdMap.set('hyperevm', '999')
 
 export function isHub(network: string): boolean {
   return network == 'base'
