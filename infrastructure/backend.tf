912e4f850f85f7316379950137e7d9ad542f694b
halaprix <halaprix@users.noreply.github.com>
2026-08-06 22:37:59 +0200

    chore: update s3 backend bucket name for terraform state storage

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "summer-earn-protocol-tfstate-17c8a2ef"
+    bucket       = "summer-earn-protocol-tfstate-026090545685"
     key          = "global/s3/terraform.tfstate"
     region       = "eu-central-1"
     encrypt      = true
